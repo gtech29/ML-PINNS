0cc9f72999974649b10288edddf3c0af77f2b958
gtech29 <juan.rodriguez.517@my.csun.edu>
2024-05-31 08:58:30 -0700

    update

--- tensor.py
+++ tensor.py
@@ -1,8 +1,9 @@
 import torch
 import numpy as np
-np.set_printoptions(precision=3) 
+
+np.set_printoptions(precision=3)
 a = [1, 2, 3]
-b = np.array([4, 5, 6], dtype=np.int32) 
+b = np.array([4, 5, 6], dtype=np.int32)
 t_a = torch.tensor(a)
 t_b = torch.from_numpy(b)
 # print(t_a)
@@ -10,8 +11,8 @@ t_b = torch.from_numpy(b)
 
 t_ones = torch.ones(2, 3)
 t_ones.shape
-torch.Size([2,3])
+torch.Size([2, 3])
 # print(t_ones)
 
-rand_tensor = torch.rand(2,3)
+rand_tensor = torch.rand(2, 3)
 # print(rand_tensor)
