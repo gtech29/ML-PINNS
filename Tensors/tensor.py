eb393ed1134e0dd125d39aa8d6f902dd3563a88f
gtech29 <juan.rodriguez.517@my.csun.edu>
2024-05-31 09:46:00 -0700

    update

--- Tensors/tensor.py
+++ Tensors/tensor.py
@@ -8,15 +8,15 @@ a = [1, 2, 3]
 b = np.array([4, 5, 6], dtype=np.int32)
 t_a = torch.tensor(a)
 t_b = torch.from_numpy(b)
-# print(t_a)
-# print(t_b)
+print(t_a)
+print(t_b,"\n")
 
 # Tensor with a specific shape
 t_ones = torch.ones(2, 3)
 t_ones.shape 
 torch.Size([2, 3])
-print(t_ones)
+print(t_ones,"\n")
 
-
+# Create a tensor with random values:
 rand_tensor = torch.rand(2, 3)
-# print(rand_tensor)
+print(rand_tensor, "\n")
