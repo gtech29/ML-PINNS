a87070fa572436e1c664e7e65d0097b926c456f9
gtech29 <juan.rodriguez.517@my.csun.edu>
2024-06-07 15:09:56 -0700

    update

--- Scripts/Data Preprocessing/demo.py
+++ Scripts/Data Preprocessing/demo.py
@@ -1,61 +1,23 @@
-﻿from pyexpat import model
-from turtle import forward
-import torch
+﻿import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# check if GPU is available and use it; otherwise use CPU
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-
 # Create a Model Class that inherits nn.Module
-class Network(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.hidden_layer = nn.Linear(1, 10)
-        self.output_layer = nn.Linear(10, 1)
-
-    def forward(self, x):
-        layer_out = torch.sigmoid(self.hidden_layer(x))
-        output = self.output_layer(layer_out)
-        return output
-
-N = Network()
-N = N.to(device)
-
-def f(x):
-    return torch.exp(x)
-
-def loss(x):
-    x.requires_grad = True
-    y = N(x)
-    dy_dx = torch.autograd.grad(y.sum(), x, create_graph=True)[0]
+class Model(nn.Module):
+    def __init__(self, in_features=4, h1= 8, h2= 9, out_features= 3):
+        super().__init__()# instantiate nn.Module
+        self.fc1 = nn.Linear(in_features, h1)
+        self.fc2 = nn.Linear(h1.h2)
+        self.out = nn.Linear(h2, out_features)
+        
+    def forward(self,x):
+        x = F.relu(self.fc1(x))
+        X = F.relu(self.fc2(x))
+        X = self.out(x)
+        
+        return x
     
-    return torch.mean( (dy_dx - f(x))**2 ) + (y[0, 0] - 1.)**2
-optimizer = torch.optim.LBFGS(N.parameters())
-
-x = torch.linspace(0, 1, 100)[:, None]
-
-def closure():
-    optimizer.zero_grad()
-    l = loss(x)
-    l.backward()
-
-    return l
-
-epochs = 10
-for i in range(epochs):
-    optimizer.step(closure)
-import matplotlib.pyplot as plt
-
-xx = torch.linspace(0, 1, 100)[:, None]
-with torch.no_grad():
-    yy = N(xx)
-
-plt.figure(figsize=(10, 6))
-plt.plot(xx, yy, label="Predicted")
-plt.plot(xx, torch.exp(xx), '--', label="Exact")
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend()
-plt.grid()
+# Pick a manual seed for randomization
+torch.manual_seed(41)
+# Create an instance of the model
+model = Model()
